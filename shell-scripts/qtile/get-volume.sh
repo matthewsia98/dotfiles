dadaec97ed29f8155bd4788e6e369584f57a8afe
Matthew Sia <matthewsia98@gmail.com>
2022-07-28 00:22:57 -0400

    modified to work for mute

--- shell-scripts/qtile/get-volume.sh
+++ shell-scripts/qtile/get-volume.sh
@@ -1,3 +1,10 @@
 #!/bin/zsh
+muted=$(pactl get-sink-mute @DEFAULT_SINK@)
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o -E '([0-9]*)%' | head -1)
-printf '%s%s%s' '[' $volume ']'
+print $muted
+if [[ $muted = *"yes"* ]] || [[ $volume == '0%' ]]
+then
+    print -1
+else
+    printf '%s%s%s' '[' $volume ']'
+fi
