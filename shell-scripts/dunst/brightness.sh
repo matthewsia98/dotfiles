34217bbbff1e628fa3780ca18d1e21b76839a168
Matthew Sia <matthewsia98@gmail.com>
2022-08-01 21:24:25 -0400

    suppressed output of brightnessctl

--- shell-scripts/dunst/brightness.sh
+++ shell-scripts/dunst/brightness.sh
@@ -2,13 +2,13 @@
 
 if [[ $1 == "raise" ]]
 then
-    brightnessctl s 5%+
+    brightnessctl -q s 5%+
 elif [[ $1 == "lower" ]]
 then
-    brightnessctl s 5%-
+    brightnessctl -q s 5%-
 fi
 
-curr_brightness=$(brightnessctl get)
-max_brightness=$(brightnessctl max)
+curr_brightness=$(brightnessctl -q get)
+max_brightness=$(brightnessctl -q max)
 brightness=$(( curr_brightness * 100 / max_brightness ))
 dunstify -h string:x-dunst-stack-tag:brightness "Brightness: [$brightness%]" -h int:value:"$brightness" -t 1500
