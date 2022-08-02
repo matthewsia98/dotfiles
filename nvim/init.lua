1294c17d2ae5ecb63dc25939c2986ffea890545d
Matthew Sia <matthewsia98@gmail.com>
2022-08-02 08:02:01 -0400

    added lspconfig for python and lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,6 +117,7 @@ Plug('rhysd/git-messenger.vim')
 Plug('numToStr/Comment.nvim')
 Plug('norcalli/nvim-colorizer.lua')
 Plug('tpope/vim-surround')
+Plug('neovim/nvim-lspconfig')
 Plug('windwp/nvim-autopairs')
 vim.call('plug#end')
 
@@ -190,6 +191,56 @@ require('Comment').setup({
 
 -- COLORIZER --
 require('colorizer').setup()
+
+-- LSP CONFIG --
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+                      -- Enable completion triggered by <c-x><c-o>
+                      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                      -- Mappings.
+                      -- See `:help vim.lsp.*` for documentation on any of the below functions
+                      local bufopts = { noremap=true, silent=true, buffer=bufnr }
+                      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+                      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                      vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
+                      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+                      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+                      vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+                      vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+                      vim.keymap.set('n', '<space>wl', 
+                                     function()
+                                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                                     end, bufopts)
+                      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+                      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+                      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+                      vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+                      vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+                  end
+
+local lsp_flags = {
+    debounce_text_changes = 150,
+}
+
+require('lspconfig')['pyright'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
+require('lspconfig')['sumneko_lua'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
 
 -- AUTOPAIRS --
 require('nvim-autopairs').setup()
