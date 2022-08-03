2990b95b2dca3f059be9ddd505248c76c9ba7f56
Matthew Sia <matthewsia98@gmail.com>
2022-08-03 08:58:21 -0400

    set up mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,6 +118,7 @@ Plug('numToStr/Comment.nvim')
 Plug('norcalli/nvim-colorizer.lua')
 Plug('tpope/vim-surround')
 Plug('neovim/nvim-lspconfig')
+Plug('williamboman/mason.nvim')
 Plug('windwp/nvim-autopairs')
 vim.call('plug#end')
 
@@ -196,10 +197,10 @@ require('colorizer').setup()
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -214,18 +215,18 @@ local on_attach = function(client, bufnr)
                       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
                       vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
                       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-                      vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-                      vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-                      vim.keymap.set('n', '<space>wl', 
+                      vim.keymap.set('n', '<leader>s', vim.lsp.buf.signature_help, bufopts)
+                      vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+                      vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+                      vim.keymap.set('n', '<leader>wl', 
                                      function()
                                         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                                      end, bufopts)
-                      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-                      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-                      vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+                      vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+                      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+                      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
                       vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-                      vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+                      vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
                   end
 
 local lsp_flags = {
@@ -241,6 +242,12 @@ require('lspconfig')['sumneko_lua'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
 }
+
+-- MASON --
+require('mason').setup({
+
+                       }
+                      )
 
 -- AUTOPAIRS --
 require('nvim-autopairs').setup()
