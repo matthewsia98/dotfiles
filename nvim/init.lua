589950028feb36b73f80322800c588845a7d68bd
Matthew Sia <matthewsia98@gmail.com>
2022-08-04 00:58:31 -0400

    added keybinds for git messenger

--- nvim/init.lua
+++ nvim/init.lua
@@ -292,6 +292,14 @@ require('gitsigns').setup {
 -- GIT MESSENGER --
 vim.g.git_messenger_no_default_mappings = true
 vim.g.git_messenger_always_into_popup = true
+-- q 	Close the popup window
+-- o 	older. Back to older commit at the line
+-- O 	Opposite to o. Forward to newer commit at the line
+-- d 	Toggle unified diff hunks only in current file of the commit
+-- D 	Toggle all unified diff hunks of the commit
+-- r 	Toggle word diff hunks only in current file of the commit
+-- R 	Toggle all word diff hunks of current commit
+-- ? 	Show mappings help
 
 -- COMMENT PLUGIN --
 require('Comment').setup({
