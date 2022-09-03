e3fc5f73453bfb893eade38ca5f1b5ac465fa9e6
Matthew Sia <matthewsia98@gmail.com>
2022-09-03 04:41:27 -0400

    added symlinks script

---
+++ shell-scripts/symlinks.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+ln -s ~/.dotfiles/conky ~/.config/conky
+ln -s ~/.dotfiles/dunst ~/.config/dunst
+ln -s ~/.dotfiles/gtk-3.0 ~/.config/gtk-3.0
+ln -s ~/.dotfiles/kitty ~/.config/kitty
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/pcmanfm ~/.config/pcmanfm
+ln -s ~/.dotfiles/picom ~/.config/picom
+ln -s ~/.dotfiles/qtile ~/.config/qtile
+ln -s ~/.dotfiles/rofi ~/.config/rofi
+ln -s ~/.dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -s ~/.dotfiles/sxhkd ~/.config/sxhkd
