2b646941cfa978e96ead2b3d0b4adc2831b1b3ec
Matthew Sia <matthewsia98@gmail.com>
2022-11-07 20:44:42 -0500

    fixed copilot keymaps

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -1,8 +1,7 @@
 local keys = require('user.keymaps')
 
 -- Next and Prev mappings in ~/.config/nvim/lua/user/plugins/lsp/luasnip.lua
-vim.cmd [[ imap <silent><script><expr> <C-Space> copilot#Accept("\<CR>") ]]
-keys.map('i', '<C-c>', function()
-    vim.cmd [[ call copilot#Dismiss() ]]
-end)
+-- vim.cmd [[ imap <silent><script><expr> <C-Space> copilot#Accept() ]]
+keys.map('i', '<C-Space>', 'copilot#Accept()', { expr = true })
+keys.map('i', '<C-c>', 'copilot#Dismiss()', { expr = true })
 vim.g.copilot_no_tab_map = true
