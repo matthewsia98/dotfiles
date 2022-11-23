b4330121cdfc7773617f092c010e130694c43630
Matthew Sia <matthewsia98@gmail.com>
2022-11-23 16:44:01 -0500

    added send visual lines to terminal

--- nvim/.config/nvim/lua/user/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/user/plugins/toggleterm.lua
@@ -2,8 +2,12 @@ local installed, toggleterm = pcall(require, "toggleterm")
 
 if installed then
     toggleterm.setup({
-        -- size = 100,
-        direction = "float",
+        size = function(term)
+            if term.direction == "vertical" then
+                return vim.o.columns * 0.4
+            end
+        end,
+        direction = "vertical",
         shell = "/bin/zsh",
         float_opts = {
             border = "rounded",
@@ -15,6 +19,8 @@ if installed then
 
     local keys = require("user.keymaps")
     keys.map("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+    -- keys.map("n", "<leader>tt", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle Floating Terminal" })
+    -- keys.map("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Toggle Vertical Split Terminal" })
     keys.map("n", "<leader>tr", function()
         local filetype = vim.bo.filetype
         local filepath = vim.fn.expand("%")
@@ -35,8 +41,16 @@ if installed then
             local prefix = #splits == 1 and "./" or ""
             command = "chmod +x " .. filepath .. "; " .. prefix .. filepath
         end
-        -- local winwidth = vim.fn.winwidth(0)
-        -- vim.cmd('TermExec size=' .. math.floor(winwidth / 3) .. ' cmd="' .. command .. '"')
         vim.cmd('TermExec cmd="' .. command .. '"')
     end, { desc = "Run File" })
+    keys.map("v", "<leader>tr", function()
+        if vim.bo.filetype == "python" then
+            local ipython = vim.fn.system("ps -f -u $USER | grep '[i]python --no-autoindent'")
+            if ipython == "" then
+                vim.cmd('TermExec cmd="ipython --no-autoindent"')
+            end
+        end
+        local cmd = [[<Esc><cmd>lua require('toggleterm').send_lines_to_terminal('visual_lines', false, {})<CR>]]
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd, true, false, true), "v", false)
+    end, { desc = "Send Lines to Terminal" })
 end
