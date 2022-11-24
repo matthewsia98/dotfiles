e811ed9e1472b7bdd99bb6fb8fc291dfa5d28e4d
Matthew Sia <matthewsia98@gmail.com>
2022-11-24 02:35:27 -0500

    added function to send lines to terminal

--- nvim/.config/nvim/lua/user/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/user/plugins/toggleterm.lua
@@ -1,4 +1,5 @@
 local installed, toggleterm = pcall(require, "toggleterm")
+local M = {}
 
 if installed then
     toggleterm.setup({
@@ -43,24 +44,8 @@ if installed then
         end
         vim.cmd('TermExec cmd="' .. command .. '"')
     end, { desc = "Run File" })
-    keys.map("v", "<leader>tr", function()
-        local filetype = vim.bo.filetype
-        local t = require('toggleterm.terminal')
-        if #t.get_all() == 0 then
-            t.get_or_create_term(t.get_toggled_id(), nil, nil):open(nil, nil)
-            local term = t.get_all()[t.get_toggled_id()]
-            local job = term.job_id
-            local pid = vim.fn.jobpid(job)
-            vim.g.toggleterm_pid = pid
-        end
 
-        if filetype == "python" then
-            local ipython = vim.fn.system("ps -f -u $USER | grep '" .. vim.g.toggleterm_pid .. ".*[i]python --no-autoindent'")
-            if ipython == "" then
-                vim.cmd('TermExec cmd="ipython --no-autoindent"')
-            end
-        end
-        local cmd = [[<Esc><cmd>lua require('toggleterm').send_lines_to_terminal('visual_lines', false, {})<CR>]]
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd, true, false, true), "v", false)
-    end, { desc = "Send Lines to Terminal" })
+    keys.map("v", "<leader>tr", "<Esc><cmd>lua require('user.functions').send_visual_lines_to_terminal()<CR>")
 end
+
+return M
