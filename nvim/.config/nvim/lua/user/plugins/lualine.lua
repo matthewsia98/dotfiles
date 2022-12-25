f42bc5058666296645751844d052f8c796f175fb
Matthew Sia <matthewsia98@gmail.com>
2022-12-25 11:48:46 -0500

    added diagnostic msg component

--- nvim/.config/nvim/lua/user/plugins/lualine.lua
+++ nvim/.config/nvim/lua/user/plugins/lualine.lua
@@ -142,7 +142,32 @@ if installed then
                     separator = separator(),
                 },
             },
-            lualine_c = {},
+            lualine_c = {
+                -- {
+                --     function()
+                --         local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+                --         local max_severity = 4
+                --         local max_severity_idx
+                --         for i, diagnostic in ipairs(diagnostics) do
+                --             if diagnostic.severity < max_severity then
+                --                 max_severity_idx = i
+                --             end
+                --         end
+                --         return diagnostics[max_severity_idx].message
+                --     end,
+                --     color = function()
+                --         local severities = { "Error", "Warn", "Info", "Hint" }
+                --         local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+                --         local max_severity = 4
+                --         for _, diagnostic in ipairs(diagnostics) do
+                --             if diagnostic.severity < max_severity then
+                --                 max_severity = diagnostic.severity
+                --             end
+                --         end
+                --         return "Diagnostic" .. severities[max_severity]
+                --     end
+                -- }
+            },
             lualine_x = {},
             lualine_y = {
                 {
