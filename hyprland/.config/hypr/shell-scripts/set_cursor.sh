0ef63866b358b4f7f1c52539f3234121dfba7c8c
Matthew Sia <matthewsia98@gmail.com>
2023-02-27 06:08:23 -0500

    fix scaling

--- hyprland/.config/hypr/shell-scripts/set_cursor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-gnome_schema="org.gnome.desktop.interface"
-cursor_theme=$(gsettings get "$gnome_schema" cursor-theme)
-cursor_size=$(gsettings get "$gnome_schema" cursor-size)
-cursor_theme=$(sed -e "s/^'//" -e "s/'$//" <<< "$cursor_theme")
-
-hyprctl setcursor "$cursor_theme" "$cursor_size"
