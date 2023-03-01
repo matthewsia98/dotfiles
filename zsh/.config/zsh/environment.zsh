a0a8f5521230a8bfda0bd465b4990040c0a4f2df
Matthew Sia <matthewsia98@gmail.com>
2023-03-01 02:18:28 -0500

    enable scrolling for git delta pager

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -8,11 +8,16 @@ export VISUAL="nvim"
 export PSQL_EDITOR="nvim"
 
 export HISTFILE=~/.zsh_history
+
 # Session History
 export HISTSIZE=100000
+
 # Max history file size
 export SAVEHIST=1000000000
+
 # Add timestamps
 export HISTTIMEFORMAT="[%F %T]"
 
 export EXA_COLORS="*.png=35"
+
+export DELTA_PAGER="less -RF --mouse --wheel-lines=4"
