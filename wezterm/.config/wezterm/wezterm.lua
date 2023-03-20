4afec63be1016315a75800068d7e7d6ecdee88db
Matthew Sia <matthewsia98@gmail.com>
2023-03-20 16:07:30 -0400

    configure wezterm tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,23 +1,38 @@
 local wezterm = require("wezterm")
 
-local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-custom.tab_bar.background = "#11111B"
-custom.tab_bar.active_tab.bg_color = "#CBA6F7"
-custom.tab_bar.active_tab.fg_color = "#11111B"
-custom.tab_bar.inactive_tab.bg_color = "#181825"
-custom.tab_bar.inactive_tab.fg_color = "#CDD6F4"
-custom.tab_bar.new_tab.bg_color = "#11111B"
+local catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+catppuccin.tab_bar.background = "#1E1E2E"
+catppuccin.tab_bar.active_tab.bg_color = "#11111B"
+catppuccin.tab_bar.inactive_tab.bg_color = "#1E1E2E"
 
 return {
-    font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", italic = true }),
+    font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
     font_size = 12.0,
 
-    color_schemes = {
-        ["custom"] = custom,
+    color_schemes = { ["catppuccin"] = catppuccin },
+    color_scheme = "catppuccin",
+    colors = {
+        tab_bar = {
+            active_tab = {
+                bg_color = catppuccin.tab_bar.active_tab.bg_color,
+                fg_color = catppuccin.tab_bar.active_tab.fg_color,
+            },
+            inactive_tab = {
+                bg_color = catppuccin.tab_bar.inactive_tab.bg_color,
+                fg_color = catppuccin.tab_bar.inactive_tab.fg_color,
+            },
+            new_tab = {
+                bg_color = catppuccin.tab_bar.new_tab.bg_color,
+                fg_color = catppuccin.tab_bar.new_tab.fg_color,
+            },
+        },
     },
-    color_scheme = "custom",
 
-    use_fancy_tab_bar = false,
+    use_fancy_tab_bar = true,
+    window_frame = {
+        active_titlebar_bg = catppuccin.tab_bar.background,
+        inactive_titlebar_bg = catppuccin.tab_bar.background,
+    },
 
     keys = {
         {
