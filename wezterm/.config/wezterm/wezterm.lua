d40018640bef417144c97a28e7f2666be8630044
Matthew Sia <matthewsia98@gmail.com>
2023-04-04 05:38:27 -0400

    add smart-splits.nvim integration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,6 +4,51 @@ local catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 catppuccin.tab_bar.background = catppuccin.background
 catppuccin.tab_bar.active_tab.bg_color = "#89B4FA"
 
+-- Equivalent to POSIX basename(3)
+-- Given "/foo/bar" returns "bar"
+-- Given "c:\\foo\\bar" returns "bar"
+local function basename(s)
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+local function is_vim(pane)
+    local process_name = basename(pane:get_foreground_process_name())
+    return process_name == "nvim" or process_name == "vim"
+end
+
+local direction_keys = {
+    LeftArrow = "Left",
+    DownArrow = "Down",
+    UpArrow = "Up",
+    RightArrow = "Right",
+
+    h = "Left",
+    j = "Down",
+    k = "Up",
+    l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+    return {
+        key = key,
+        mods = "CTRL",
+        action = wezterm.action_callback(function(win, pane)
+            if is_vim(pane) then
+                -- pass the keys through to vim/nvim
+                win:perform_action({
+                    SendKey = { key = key, mods = "CTRL" },
+                }, pane)
+            else
+                if resize_or_move == "resize" then
+                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+                else
+                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+                end
+            end
+        end),
+    }
+end
+
 return {
     term = "wezterm",
 
@@ -84,5 +129,17 @@ return {
                 mode = "SwapWithActive",
             }),
         },
+
+        -- move between split panes
+        split_nav("move", "h"),
+        split_nav("move", "j"),
+        split_nav("move", "k"),
+        split_nav("move", "l"),
+
+        -- resize panes
+        split_nav("resize", "LeftArrow"),
+        split_nav("resize", "DownArrow"),
+        split_nav("resize", "UpArrow"),
+        split_nav("resize", "RightArrow"),
     },
 }
