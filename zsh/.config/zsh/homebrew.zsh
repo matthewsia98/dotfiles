d9a1c5a1c9a9499968784d5cc85a26c8fa48298e
Matthew Sia <matthewsia98@gmail.com>
2023-04-05 02:17:46 -0400

    idk

--- zsh/.config/zsh/homebrew.zsh
+++ zsh/.config/zsh/homebrew.zsh
@@ -3,4 +3,6 @@ export HOMEBREW_PREFIX="$(brew --prefix)"
 export PATH="${HOMEBREW_PREFIX}/bin:${PATH}"
 export PATH="${HOMEBREW_PREFIX}/opt/postgresql@15/bin:${PATH}"
 
+export CC="${HOMEBREW_PREFIX}/bin/gcc-12"
+
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
