f00572c88d998248459d301fe85f4368e43625ab
Matthew Sia <matthewsia98@gmail.com>
2023-05-04 20:46:59 -0400

    add miniconda setup to homebrew.zsh

--- zsh/.config/zsh/homebrew.zsh
+++ zsh/.config/zsh/homebrew.zsh
@@ -3,7 +3,7 @@ export HOMEBREW_PREFIX="$(/opt/homebrew/bin/brew --prefix)"
 export PATH="${HOMEBREW_PREFIX}/bin:${PATH}"
 export PATH="${HOMEBREW_PREFIX}/opt/postgresql@15/bin:${PATH}"
 
-export CC="${HOMEBREW_PREFIX}/bin/gcc-12"
+export CC="${HOMEBREW_PREFIX}/bin/gcc-13"
 
 FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
 FPATH="${HOMEBREW_PREFIX}/share/zsh/zsh-completions:${FPATH}"
@@ -11,3 +11,22 @@ FPATH="${HOMEBREW_PREFIX}/share/zsh/zsh-completions:${FPATH}"
 alias brewin="brew install"
 alias brewrm="brew uninstall"
 alias brewg="brew list --versions | grep"
+alias brewsyu="brew update && brew upgrade"
+
+alias checkupdates="brew outdated"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
