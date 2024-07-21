284baa7fbceaebabbe7e05b0e10aef4e0b27a29b
Matthew Sia <matthewsia98@gmail.com>
2024-07-20 20:51:22 -0400

    use oil instead of netrw for directories

--- nvim/.config/lazyvim/lua/config/lazy.lua
+++ nvim/.config/lazyvim/lua/config/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
                 "gzip",
                 -- "matchit",
                 -- "matchparen",
-                -- "netrwPlugin",
+                "netrwPlugin",
                 "tarPlugin",
                 "tohtml",
                 "tutor",
--- nvim/.config/lazyvim/lua/plugins/oil.lua
+++ nvim/.config/lazyvim/lua/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
     "stevearc/oil.nvim",
     cmd = { "Oil" },
+    init = function()
+        if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+            require("oil")
+        end
+    end,
     opts = {
         keymaps = {
             ["q"] = "actions.close",
